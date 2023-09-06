e189c4032ac65c24c81a6400f4bb5f768f51a7b6
Ras <rasrahim823@gmail.com>
2023-09-05 22:19:50 -0400

    temperature test

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,5 +1,9 @@
-
 def convert_100_to_celsius():
+    celsius_100 = ((100-32) * 5/9)
+    print(celsius_100)
+    print(type(celsius_100))
+    #The number contains a deciamak, so it is a float.
+convert_100_to_celsius()
     # Convert a temperature of 100 degrees fahrenheit to celsius
     # Save this to a variable called celsius_100, and use print() to print out the value
     # Is the resulting temperature you get an integer or float?
@@ -7,30 +11,51 @@ def convert_100_to_celsius():
     # How do you know? Write a comment below your code explaining how you know which it is
 
 #convert_100_to_celsius()
+    
+    
+#The decimal indicates that it is a float
 
 def convert_0_to_celsius():
+    celsius_0 = ((0-32)*5/9)
+    print(celsius_0)
+convert_0_to_celsius()
     # Convert a temperature of 0 degrees fahrenheit to celsius
     # Save this to a variable called celsius_0, and use print() to print out the value
 
 #convert_0_to_celsius()
-
+    
+    
 def convert_34_2_to_celsius():
+    print((34.2-32)*5/9)
+convert_34_2_to_celsius()
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
     
 #convert_34_2_to_celsius()
 
+    
 
 # Now, can you convert back?
 
 
 def convert_5_to_fahrenheit():
+    print((5*9/5)+32)
+convert_5_to_fahrenheit()
     # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
 
 #convert_5_to_fahrenheit()
 
-def hotter_temp():
+    
+    
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
 
 #hotter_temp()
+def hotter_temp():
+    print ('30.2 degrees celsius')
+hotter_temp() 
+    
+    
+
+
+
