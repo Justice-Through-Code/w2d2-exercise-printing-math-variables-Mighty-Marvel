cc646691a5373040183c340385ce37dad4f635e8
Ras <rasrahim823@gmail.com>
2023-09-05 23:05:51 -0400

    newest attempt

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,38 +1,38 @@
+
 def convert_100_to_celsius():
-    celsius_100 = (100-32) * 5/9
-    print(celsius_100)
-    print(type(celsius_100))
-    #The number contains a decimal, so it is a float.
-convert_100_to_celsius()
     # Convert a temperature of 100 degrees fahrenheit to celsius
     # Save this to a variable called celsius_100, and use print() to print out the value
     # Is the resulting temperature you get an integer or float?
     # Print 'float' if it is a float or 'int' if it is an int
     # How do you know? Write a comment below your code explaining how you know which it is
-
+    celsius_100 = (100-32) * 5/9
+    print(celsius_100)
+    print(type(celsius_100))
+    #The number contains a decimal, so it is a float.
+convert_100_to_celsius()
 #convert_100_to_celsius()
     
     
 #The decimal indicates that it is a float
 
 def convert_0_to_celsius():
+   
+    # Convert a temperature of 0 degrees fahrenheit to celsius
+    # Save this to a variable called celsius_0, and use print() to print out the value
     celsius_0 = (0-32)*5/9
     print(celsius_0)
 
 convert_0_to_celsius()
-    # Convert a temperature of 0 degrees fahrenheit to celsius
-    # Save this to a variable called celsius_0, and use print() to print out the value
-
 #convert_0_to_celsius()
     
     
 def convert_34_2_to_celsius():
-    print((34.2-32)*5/9)
-
-convert_34_2_to_celsius()
+   
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
-    
+    print((34.2-32)*5/9)
+
+convert_34_2_to_celsius()    
 #convert_34_2_to_celsius()
 
     
@@ -43,9 +43,9 @@ convert_34_2_to_celsius()
 def convert_5_to_fahrenheit():
     print((5*9/5)+32)
 
-convert_5_to_fahrenheit()
-# Convert a temperature of 5 degrees celsius to fahrenheit and print it out
 
+# Convert a temperature of 5 degrees celsius to fahrenheit and print it out
+convert_5_to_fahrenheit()
 #convert_5_to_fahrenheit()
 
     
@@ -55,7 +55,7 @@ convert_5_to_fahrenheit()
 
 #hotter_temp()
 def hotter_temp():
-    print ('30.2 degrees celsius')
+    print ("30.2 degrees celsius")
 
 hotter_temp() 
     
